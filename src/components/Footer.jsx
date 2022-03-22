00861039a8beb0a28100d574bf20a21326f40dfa
Rohit Gulam <rohitsgulam@gmail.com>
2022-03-22 10:47:06 +0300

    styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,21 +9,27 @@ function Footer() {
     <FooterDiv>
         <div>
             <NavLink to='/' className="site-title">
-                <img src={logo} alt="Student Developer Community Logo" width="140px" height="80px"/>
+                <img src={logo} alt="Student Developer Community Logo" width="105px" height="60px"/>
             </NavLink>
             <Button text="Join the community" />
         </div>
+        <hr />
+        
         <LFooter>
-            <p>Made with 💜 </p>
+            <p className='small' > &copy; Student Developers Community. All rights reserved </p>
         </LFooter>
     </FooterDiv>
   )
 }
 
 const FooterDiv = styled.footer`
-    background-color: #1d3557;
+    background-color: #1f3a68;
     color: white;
     padding: 10px 0px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 
     div{
         width: 80%;
@@ -32,11 +38,19 @@ const FooterDiv = styled.footer`
         align-items: center;
         justify-content: space-between;
     }
+
+    hr{
+        width: 80%;
+        border: 0.8 solid #d9dbde;
+        margin: 10px 0;
+    }
 `;
 
 const LFooter = styled.div`
     width: 100%;
-    p{
+
+    .small{
+        font-size: 0.8rem;
         text-align: center;
         width: 100%;
     }
